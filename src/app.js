4c8f5cc0dfb8807de997600a6fe6f14b3eff382f
zaturnvalley <zacharycmason@gmail.com>
2017-12-20 14:36:47 -0800

    pulled in login form

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount(){
@@ -14,12 +15,12 @@ class App extends Component {
     messagingSenderId: '868857582456'
     });
   }
-
+gi
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>App</Text>
+        <LoginForm />
       </View>
     );
   }
