6770d5e8a04c9dc2dc7a1751b49a0833523c128a
zaturnvalley <zacharycmason@gmail.com>
2017-12-20 13:20:10 -0800

    imported firebase and componentwillmount

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component} from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp({
+    apiKey: 'AIzaSyDOGx7p8JA2S88HPdfk0bR-He43YDfZ2TY',
+    authDomain: 'authentication-reactnati-5bcc5.firebaseapp.com',
+    databaseURL: 'https://authentication-reactnati-5bcc5.firebaseio.com',
+    projectId: 'authentication-reactnati-5bcc5',
+    storageBucket: 'authentication-reactnati-5bcc5.appspot.com',
+    messagingSenderId: '868857582456'
+    });
+  }
+
   render() {
     return (
       <View>
