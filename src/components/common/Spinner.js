6db78c4eb3670886fe16cb95336a29429e2efca8
zaturnvalley <zacharycmason@gmail.com>
2017-12-22 13:11:51 -0800

    styles to spinner

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
 const Spinner = () => {
   return (
-    <View />
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator />
+    </View>
   );
-}
+};
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
 
 export { Spinner };
