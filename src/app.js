a2a20a2b7e61d97745c5cd3bc0dbd7bcd9e28757
zaturnvalley <zacharycmason@gmail.com>
2017-12-28 16:39:19 -0800

    refactored with switch statement

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React, { Component} from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
-  state = { loggedIn: false };
+  state = { loggedIn: null };
 
   componentWillMount(){
     firebase.initializeApp({
@@ -28,15 +28,14 @@ class App extends Component {
   }
 
   renderContent() {
-    if (this.state.loggedIn){
-      return (
-        <Button>
-          Log Out
-        </Button>
-      );
+    switch(this.state.loggedIn){
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />
     }
-
-    return <LoginForm />;
   }
 
   render() {
